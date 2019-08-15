cb5d33c0dfb6c7d7adc9ef408e1dddb6fad22e73
Antoine Savignac <antoine.savignac@mylocalheroes.com>
2019-08-15 13:43:32 +0100

    Use jest and add README

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-const expect = require('expect.js');
 const game = require('./game');
 
 describe('A tennis game', () => {
   
   it('should initialise a game of tennis', () => {
-    expect(game.getScore()).to.equal('0-0');
+    expect(game.getScore()).toEqual('0-0');
   });
 
 });
